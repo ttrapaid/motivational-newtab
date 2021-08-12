35e39f5e2d2da40b53716b65c380f69fe9e48682
Dennis Johns <johnsd@union.edu>
2021-08-12 13:57:29 -0400

    Creating new tab that we then change the background color to red

---
+++ newTabRedPush/script.js
@@ -0,0 +1,10 @@
+
+
+// chrome.wallpaper.setWallpaper(
+//   {
+//     'url': 'http://i.stack.imgur.com/vhoa0.jpg',
+//     'layout': 'CENTER_CROPPED',
+//     'filename': 'test_wallpaper'
+//   },
+//   function() {}
+// );
